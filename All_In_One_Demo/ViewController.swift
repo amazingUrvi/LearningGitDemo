4c9aa89b9300a13f3f5980cf7acd681e05f60a85
UrvishaMobileFirst <urvisha.mobilefirst@gmail.com>
2022-07-04 10:19:24 +0530

    first Commit

---
+++ All_In_One_Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  All_In_One_Demo
+//
+//  Created by Apple on 04/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
